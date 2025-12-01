3d3d6aa24d366bd45fe5d2b5c9c57f88a8b577fd
mugdha <mugdhah24@gmail.com>
2025-12-02 00:46:05 +0530

    Api gateway

---
+++ api-gateway/src/main/java/com/nagarro/apigateway/APIGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.nagarro.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class APIGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(APIGatewayApplication.class, args);
+	}
+
+}
