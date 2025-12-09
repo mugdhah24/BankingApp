64ab579aa67e38b34fcc33f631c8e013dae160e5
mugdha <mugdhah24@gmail.com>
2025-12-09 13:05:29 +0530

    Updated

--- account-service/src/main/java/com/nagarro/accountservice/controller/AccountController.java
+++ account-service/src/main/java/com/nagarro/accountservice/controller/AccountController.java
@@ -8,20 +8,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/accounts")
 public class AccountController {
     private final AccountService service;
-    public AccountController(AccountService service) { this.service = service; }
+    public AccountController(AccountService service) {
+        this.service = service;
+    }
 
     @PostMapping("/{customerId}")
-    public Account create(@PathVariable Long customerId) { return service.create(customerId); }
+    public Account create(@PathVariable Long customerId) {
+        return service.create(customerId);
+    }
 
     @PostMapping("/deposit/{id}")
-    public Account deposit(@PathVariable Long id, @RequestParam Double amount) { return service.deposit(id, amount); }
+    public Account deposit(@PathVariable Long id, @RequestParam Double amount) {
+        return service.deposit(id, amount);
+    }
 
     @PostMapping("/withdraw/{id}")
-    public Account withdraw(@PathVariable Long id, @RequestParam Double amount) { return service.withdraw(id, amount); }
+    public Account withdraw(@PathVariable Long id, @RequestParam Double amount) {
+        return service.withdraw(id, amount);
+    }
 
     @GetMapping("/{id}")
-    public Account get(@PathVariable Long id) { return service.get(id); }
+    public Account get(@PathVariable Long id) {
+        return service.get(id);
+    }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
--- customer-service/src/main/java/com/nagarro/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/nagarro/customerservice/controller/CustomerController.java
@@ -13,6 +13,7 @@ public class CustomerController {
     private final CustomerService customerService;
 
     public CustomerController(CustomerService customerService) {
+
         this.customerService = customerService;
     }
 
